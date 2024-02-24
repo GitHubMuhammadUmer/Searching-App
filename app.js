672977034be8d2fe1a441e1162202e616dcab7ab
Muhammad Umer <140950473+GitHubMuhammadUmer@users.noreply.github.com>
2024-02-24 15:35:17 -0800

    add

--- app.js
+++ app.js
@@ -1,63 +1,453 @@
-var mobiles = {
-    iphone: {
-        phone15: { camera: "108px", ram: "1tb", colors: ['red', 'green', 'white'] },
-        phone14: { camera: "108px", ram: "2tb", colors: ['red', 'green', 'white'] },
-        phone17: { camera: "108px", ram: "4tb", colors: ['red', 'green', 'white'] }
-    },
-    oppo: {
-        oppo15: { pname: 'Ae15', camera: "512px", ram: "2tb", colors: ['red', 'green', 'white'] },
-        oppo16: { pname: 'Ae15', camera: "512px", ram: "2tb", colors: ['red', 'green', 'white'] },
-        oppo17: { pname: 'Ae15', camera: "512px", ram: "2tb", colors: ['red', 'green', 'white'] }
-    },
-    samsung: {
-        samsung15: { pname: 'Ae15', camera: "512px", ram: "2tb", colors: ['red', 'green', 'white'] },
-        samsung16: { pname: 'Ae15', camera: "512px", ram: "2tb", colors: ['red', 'green', 'white'] },
-        samsung17: { pname: 'Ae15', camera: "512px", ram: "2tb", colors: ['red', 'green', 'white'] }
-    }
-   
-};
-var modelSelect = document.getElementById('modelSelect');
+var carlist = {
+    BMW: {
+        BMW_3Series: {
+            image: 'https://tse3.mm.bing.net/th?id=OIP.ZBRcXqv9tRVuLy_SPIscigAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '25 MPG (City), 34 MPG (Highway)',
+            Price: '$40,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        BMW_5Series: {
+            image: 'https://tse3.mm.bing.net/th?id=OIP.4exKKB6xjLt_X4IVpGjMxQAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '22 MPG (City), 29 MPG (Highway)',
+            Price: '$55,000',
+            Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        BMW_X3: {
+            
+            image: 'https://tse1.mm.bing.net/th?id=OIP.5ZZvt0fbq3-BFAzALMHVowHaE7&pid=Api&P=0&h=220',
+            Mileage: '23 MPG (City), 30 MPG (Highway)',
+            Price: '$45,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        BMW_X5: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.ncBxo2cu2K181k07YAaFrAAAAA&pid=Api&P=0&h=220',
+            Model: '2023',
+            Mileage: '20 MPG (City), 26 MPG (Highway)',
+            Price: '$60,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
 
-function submitForm(event) {
-    event.preventDefault(); // prevent form submission
-    var brand = getSelectedBrand();
-    var model = modelSelect.value;
+        },
+        BMW_7Series: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.MpM6hTddhTokhm40wP7KlgAAAA&pid=Api&P=0&h=220',
+            Model: '2022',
+            Mileage: '17 MPG (City), 24 MPG (Highway)',
+            Price: '$85,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
 
-    alert("You selected:\nBrand: " + brand + "\nModel: " + model);
-}
+        BMW_i3: {
+            image: 'https://tse2.mm.bing.net/th?id=OIP.i8pFFOn0jzAE5UwCzqIXnAAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '126 MPGe',
+            Price: '$45,000',
+            FuelType: 'Electric',
+             Color: ['black','white', 'silver'],
+            Wheel: 'Alloy',
 
-function populateModels() {
-    var brand = getSelectedBrand();
-    var models = Object.keys(mobiles[brand]);
-    var optionsHTML = '';
-
-    // Construct HTML for model options
-    for (var i = 0; i < models.length; i++) {
-        optionsHTML += '<option value="' + models[i] + '">' + models[i] + '</option>';
-    }
-
-    // Set the innerHTML of modelSelect
-    modelSelect.innerHTML = optionsHTML;
-
-    // Show model selection
-    modelDiv.style.display = 'block';
-}
-
-
-// Function to get the selected brand
-function getSelectedBrand() {
-    var brandInputs = document.getElementsByName('brand');
-    for (var i = 0; i < brandInputs.length; i++) {
-        if (brandInputs[i].checked) {
-            return brandInputs[i].value;
         }
+    },
+
+    Tesla: {
+
+        Tesla_Model3: {
+            image: 'https://tse2.mm.bing.net/th?id=OIP.ZcO5ByW_BwxzZm98rkUwNQAAAA&pid=Api&P=0&h=220',
+            Model: '2023',
+            Mileage: '250 miles (EPA estimated)',
+            Price: '$39,990',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Electric',
+            Wheel: 'Alloy',
+        },
+        Tesla_ModelS: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.q_CU-rORAB7l5G4S1ZgkxwHaEK&pid=Api&P=0&h=220',
+            Model: '2023',
+            Mileage: '370 miles (EPA estimated)',
+            Price: '$79,990',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Electric',
+            Wheel: 'Alloy',
+        },
+        Tesla_ModelX: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.rt5t1Hd5JZqJlNA0hd19CQHaEK&pid=Api&P=0&h=220',
+            Model: '2023',
+            Mileage: '325 miles (EPA estimated)',
+            Price: '$89,990',
+         Color: ['black','white', 'silver'],
+            FuelType: 'Electric',
+            Wheel: 'Alloy',
+        },
+        Tesla_ModelY: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.iYrTLn2dlMEf5lv_EnM_BgHaE8&pid=Api&P=0&h=220',
+            Model: '2021',
+            Mileage: '300 miles (EPA estimated)',
+            Price: '$49,990',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Electric',
+            Wheel: ' Alloy',
+        },
+        Tesla_Roadster: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.5QRPY2cWKt0zDzHEIjB7ogAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '620 miles (EPA estimated)',
+            Price: '$200,000',
+ Color: ['black','white', 'silver'],
+            FuelType: 'Electric',
+            Wheel: 'Alloy',
+        },
+        Tesla_Cybertruck: {
+            image: 'https://tse2.mm.bing.net/th?id=OIP.0EtqKF0lktpXxpZBogExuQAAAA&pid=Api&P=0&h=220',
+            Model: '2020',
+            Mileage: '680 miles (EPA estimated)',
+            Price: '$39,900 (starting)',
+            Color: ['black','white', 'silver'],
+            FuelType: 'Electric',
+            Wheel: 'Alloy',
+        }
+    },
+
+
+    Suzuki: {
+        Suzuki_Swift: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.Efxuvs1zA8lnhaBJUv7y4QAAAA&pid=Api&P=0&h=220',
+            Model: '2020',
+            Mileage: '30 MPG (City), 38 MPG(Highway)',
+            Price: '$18,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Steel',
+        },
+        Suzuki_Vitara: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.152SflqeOkIgGpvaJUKC8gAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '26 MPG(City), 33 MPG(Highway)',
+            Price: '$25,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+
+        },
+
+        Suzuki_Jimny: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.RP_ekPM25e8gho-Kpakz8QAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '24 MPG(City), 30 MPG(Highway)',
+            Price: '$22,000',
+             Color: ['black','white', 'silver'],
+            FuelType: ' Gasoline',
+            Wheel: 'Steel',
+        },
+
+        Suzuki_SX4: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.KeKUFZP5ajaNTnAlhNhgTQAAAA&pid=Api&P=0&h=220',
+
+
+            Model: '2023',
+            Mileage: '28 MPG(City), 35 MPG(Highway)',
+            Price: '$20,000',
+     Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+
+
+        Suzuki_Ignis: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.yw1kbzejc8vV0rqJRCZxHgAAAA&pid=Api&P=0&h=220',
+            Model: '2020',
+            Mileage: '29 MPG(City), 36 MPG(Highway)',
+            Price: '$17,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Steel',
+        },
+
+        Suzuki_Baleno: {
+            image: 'https://tse2.mm.bing.net/th?id=OIP.QHSYIrK5p9BxyiR9sgW67wAAAA&pid=Api&P=0&h=220',
+            Model: '2022',
+            Mileage: '32 MPG(City), 40 MPG(Highway)',
+            Price: '$19,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+    },
+
+
+    Hyundai: {
+        Hyundai_Elantra: {
+            image: 'https://www.thedrive.com/content/2021/07/Elantra-N_03.jpg?quality=85&width=1440&quality=70',
+            Model: '2022',
+            Mileage: '33 MPG(City), 43 MPG(Highway)',
+            Price: '$20,000',
+ Color: ['black','white', 'silver'],
+            FuelType: ' Gasoline',
+            Wheel: 'Alloy',
+        },
+
+        Hyundai_Sonata: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.fuxk_qAXLEOm4DafKiHeyAAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '28 MPG(City), 38 MPG(Highway)',
+            Price: '$25,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+
+        Hyundai_Tucson: {
+            image: 'https://www.hyundaicanada.com/-/media/hyundai/coming-soon/2024-sonata/full_bleed/desktop/img_001.jpg?h=2160&w=3840&hash=0139FF96C2927827D4075EA02E58F4FF',
+            Model: '2023',
+            Mileage: '23 MPG(City), 28 MPG(Highway)',
+            Price: '$24,000',
+     Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+
+        },
+
+        Hyundai_SantaFe: {
+            
+            image: 'https://tse1.mm.bing.net/th?id=OIP.c4nkqAwicBCgVL1RyVo7BAAAAA&pid=Api&P=0&h=220',
+            Model: '2021',
+            Mileage: '22 MPG(City), 29 MPG(Highway)',
+            Price: '$30,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+
+        Hyundai_Kona: {
+            image: 'https://tse3.mm.bing.net/th?id=OIP.1_WUIKUF8gzMF3pRE0eOjAAAAA&pid=Api&P=0&h=220',
+            Model: '2020',
+            Mileage: '27 MPG(City), 33 MPG(Highway)',
+            Price: '$21,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+
+        },
+
+        Hyundai_Veloster: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.KuNiq08kdmwdfSmfn7rPkQAAAA&pid=Api&P=0&h=220',
+            Model: '2022',
+            Mileage: '28 MPG(City), 34 MPG(Highway)',
+            Price: '$22,000',
+         Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+
+        }
+    },
+
+    Mazda: {
+
+        Mazda_3: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.bp4dBwb8tv1foTl6EncFgQAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '28 MPG (City), 36 MPG (Highway)',
+             Color: ['black','white', 'silver'],
+            Color: 'Blue',
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+
+        },
+
+        Mazda_6: {
+            image: 'https://tse3.mm.bing.net/th?id=OIP.SjwxbtIWl9uUCyUaUcjEsAAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '26 MPG (City), 35 MPG (Highway)',
+            Price: '$25,000',
+     Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        Mazda_CX5: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.BXzpHE2XqrUIxQmeZ5YCsAAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '24 MPG (City), 30 MPG (Highway)',
+            Price: '$27,000',
+         Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        Mazda_CX9: {
+            image: 'https://tse3.mm.bing.net/th?id=OIP.OoSuUIVYH5Z0JAGfzajxZAAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '22 MPG (City), 28 MPG (Highway)',
+            Price: '$35,000',
+             Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy'
+        },
+        Mazda_MX5Miata: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.36Q7oFRzJ3WRwTTpx74zCgAAAA&pid=Api&P=0&h=220',
+            ModelYear: '2024',
+            Mileage: '26 MPG (City), 34 MPG (Highway)',
+            Price: '$27,000',
+         Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        Mazda_CX30: {
+            image: 'https://tse3.mm.bing.net/th?id=OIP.SIsYmG-6261tOBGyHQJG0gAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '28 MPG (City), 33 MPG (Highway)',
+            Price: '$22,000',
+            Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+
+    },
+
+    Toyota: {
+
+        Toyota_Corolla: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.2l8eBO55ZPnQIKBsfu_dOwAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '29 MPG (City), 37 MPG (Highway)',
+            Price: '$20,000',
+            Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        Toyota_Camry: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.woqfDVVtqdK6XMIQcHysSwAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '28 MPG (City), 39 MPG (Highway)',
+            Price: '$24,000',
+            Color: 'Blue',
+            Color: ['black','white', 'silver'],
+            Wheel: 'Alloy',
+        },
+        Toyota_RAV4: {
+            image: 'https://tse4.mm.bing.net/th?id=OIP.IXV0HeKKGinCbpbFGUsuGwAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '27 MPG (City), 35 MPG (Highway)',
+            Price: '$27,000',
+            Color: ['black','white','silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        Toyota_Highlander: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.FTD5Blnk47bHJG5vt-yr6wAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '23 MPG (City), 29 MPG (Highway)',
+            Price: '$35,000',
+            Color: ['black','white', 'silver'],
+            FuelType: ' Gasoline',
+            Wheel: 'Alloy',
+        },
+        Toyota_Tacoma: {
+            image: 'https://tse2.mm.bing.net/th?id=OIP.vHMq9ur-aWwF7czIM6WuzQAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '20 MPG (City), 23 MPG (Highway)',
+            Price: '$25,000',
+            Color: ['black','white', 'silver'],
+            FuelType: 'Gasoline',
+            Wheel: 'Alloy',
+        },
+        Toyota_Prius: {
+            image: 'https://tse1.mm.bing.net/th?id=OIP.Ks8EHrqVqczhokHMOnMflwAAAA&pid=Api&P=0&h=220',
+            Model: '2024',
+            Mileage: '58 MPG (City), 53 MPG (Highway)',
+            Price: '$25,000',
+            Color: ['black','white', 'silver'],
+            FuelType: 'Hybrid',
+            Wheel: 'Alloy',
+
+        },
     }
-    return null; // No brand selected
 }
-// for(var key in mobiles){
-//     for(var key1 in mobiles[key]){
+var main = document.getElementById("main")
+var brand = document.getElementById("brand")
+var model = document.getElementById("model")
+brand.innerHTML += `<option>Select Brand Name</option>`
+model.innerHTML += `<option>Select Model Name</option>`
 
-//  for(var key2 in mobiles[key]){
 
-//     console.log(mobiles[key][key1])
-// }   }   }
+ for(var key in carlist){
+    console.log(key)
+    brand.innerHTML += `<option value='${key}'>${key}</option>`
+    for(var key1 in carlist[key]){
+
+        var obj = carlist[key][key1];
+      
+        var colorDiv = "";
+        for (var i = 0; i < obj.Color.length; i++) {
+            colorDiv += `<div class='${obj.Color[i]}></div>`
+    //  console.log(obj.Color[i])
+            
+        }
+        main.innerHTML += `<div class="col-12 col-sm-12 col-md-6 col-lg-3 mb-4">
+
+        <div class="card" style="width: 100%;">
+            <img src="${obj.image || "https://tse3.mm.bing.net/th?id=OIP.n_d2dvzb5zOdPb_dtFuh-wHaEW&pid=Api&P=0&h=220"} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${key1.toUpperCase()} (${obj.Model})</h5>
+              <p class="card-text">${obj.Mileage}</p>
+              <p class="card-text">${obj.Wheel}</p>
+              <p class="card-text">${obj.FuelType}</p>
+              <h4 class="card-text">${obj.Price}</h4>
+              <div class="d-flex gap-2>${colorDiv}</div>
+            </div>
+          </div>
+    
+    </div>`
+
+        }
+   
+}
+
+
+function setModel() {
+    model.innerHTML = "";
+    model.innerHTML += `<option>Select Model Name</option>`
+    console.log("brand", brand.value)
+for(var key in carlist[brand.value]){
+    model.innerHTML += `<option value='${key}'>${key}</option>`
+}
+}
+ var carDetail = document.getElementById('car-detail');
+function searchcar(){
+   
+    main.style.display = "none";
+    carDetail.style.display ="flex"
+    var car = carlist[brand.value][model.value];
+    carDetail.innerHTML = `
+    <div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src= "${car.image || "https://tse3.mm.bing.net/th?id=OIP.n_d2dvzb5zOdPb_dtFuh-wHaEW&pid=Api&P=0&h=220"}
+        class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body ">
+          <h5 class="card-title">${model.value.toUpperCase()}</h5>
+          <p class="card-text">${car.Mileage}</p>
+          <p class="card-text">${car.Wheel}</p>
+          <p class="card-text">${car.FuelType}</p>
+          <h4 class="card-text">${car.Price}</h4>
+         <div class="d-flex gap-2>${colorDiv}</div>
+            </div>
+      </div>
+    </div>
+  </div>`
+}
+
+function clearSearch(){
+    carDetail.style.display = "none";
+    main.style.display = "flex";
+
+}
